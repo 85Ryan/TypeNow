eefbbda50b3e98d3cb8c0161103bed66ade69d0e
iiiRyan <85ryan.blue@gmail.com>
2017-08-17 21:20:15 +0800

    Posts pagination.

--- index.php
+++ index.php
@@ -55,7 +55,11 @@
                             while ( have_posts() ) : the_post();
                             get_template_part( 'template-parts/post/content', get_post_format() );
                         endwhile;
-                        
+                        the_posts_pagination( array(
+                                'prev_text' => typenow_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous Page', 'typenow' ) . '</span>',
+                                'next_text' => '<span class="screen-reader-text">' . __( 'Next Page', 'typenow' ) . '</span>' . typenow_get_svg( array( 'icon' => 'arrow-right' ) ),
+                                'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'typenow' ) . ' </span>',
+                            ) );
                         else :
                             get_template_part( 'template-parts/post/content', 'none' );
                         endif;
