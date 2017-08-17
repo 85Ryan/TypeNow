699b3de96dbb6a9f2dd888ddab6299276df7a28a
iiiRyan <85ryan.blue@gmail.com>
2017-08-17 23:29:58 +0800

    Modify entry title.

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -23,7 +23,11 @@
             echo '</div><!-- .entry-meta -->';
         };
 
-        the_title( '<h1 class="entry-title">', '</h1>' );
+		if ( is_single() ) {
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		} else {
+			the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+		}
 
         ?>
     </header><!-- .entry-header -->
