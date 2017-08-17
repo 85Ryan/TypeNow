b3f8091564dab856214a0794a980bf387db8256f
iiiRyan <85ryan.blue@gmail.com>
2017-08-17 21:36:42 +0800

    Display site info.

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Displays footer site info.
+ */
+?>
+
+<div class="site-info">
+    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'typenow' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'typenow' ), 'WordPress' ); ?></a>
+</div><!-- .site-info -->
