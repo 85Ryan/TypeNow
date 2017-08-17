2f7a5bdc45a8402ee73618f64dc44cc1f56c9aac
iiiRyan <85ryan.blue@gmail.com>
2017-08-17 19:43:02 +0800

    Display navigation top menu.

--- index.php
+++ index.php
@@ -18,6 +18,14 @@
     
     <header id="masthead" class="site-header" role="banner">
         <?php get_template_part( 'template-parts/header/header', 'image' ); ?>
+        
+        <?php if ( has_nav_menu( 'top' ) ) : ?>
+        <div class="navigation-top">
+            <div class="wrap">
+                <?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
+            </div><!-- .wrap -->
+        </div><!-- .navigation-top -->
+        <?php endif; ?>
     </header><!-- #masthead -->
 </div><!-- #page -->
 </body>
