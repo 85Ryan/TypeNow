4f5ef91fa052330ca51e0418bc5b213ba29dcf2d
iiiRyan <85ryan.blue@gmail.com>
2017-08-17 20:22:27 +0800

    The main loop.

--- index.php
+++ index.php
@@ -27,6 +27,46 @@
         </div><!-- .navigation-top -->
         <?php endif; ?>
     </header><!-- #masthead -->
+    
+    <?php if ( ( is_single() || ( is_page() && ! typenow_is_frontpage() ) ) && has_post_thumbnail( get_queried_object_id() ) ) :
+        echo '<div class="single-featured-image-header">';
+        echo get_the_post_thumbnail( get_queried_object_id(), 'typenow-featured-image' );
+        echo '</div><!-- .single-featured-image-header -->';
+    endif;
+    ?>
+    
+    <div class="site-content-contain">
+        <div id="content" class="site-content">
+            <div class="wrap">
+                <?php if ( is_home() && ! is_front_page() ) : ?>
+                <header class="page-header">
+                    <h1 class="page-title"><?php single_post_title(); ?></h1>
+                </header>
+                <?php else : ?>
+                <header class="page-header">
+                    <h2 class="page-title"><?php _e( 'Posts', 'typenow' ); ?></h2>
+                </header>
+                <?php endif; ?>
+                
+                <div id="primary" class="content-area">
+                    <main id="main" class="site-main" role="main">
+                        <?php 
+                        if ( have_posts() ) :
+                            while ( have_posts() ) : the_post();
+                            get_template_part( 'template-parts/post/content', get_post_format() );
+                        endwhile;
+                        
+                        else :
+                            get_template_part( 'template-parts/post/content', 'none' );
+                        endif;
+                        ?>
+                    </main>
+                </div><!-- #primary .content-area -->
+            </div><!-- .wrap -->
+        </div><!-- #content .site-content -->
+    </div><!-- .site-content-contain -->
+    
 </div><!-- #page -->
+<?php wp_footer(); ?>
 </body>
 </html>
