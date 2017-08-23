b7916a908eb428ec2c05cc1164934a5734495181
iiiRyan <85ryan.blue@gmail.com>
2017-08-23 13:35:20 +0800

    Page template.

---
+++ page.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * The template for displaying all pages.
+ */
+get_header(); ?>
+<div class="wrap">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <?php 
+            while ( have_posts() ) : the_post() :
+                get_template_part( 'template-parts/page/content', 'page' );
+            
+                // If comments are open or we have at least one comment, load up the comment template.
+                if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+            endwhile;
+            ?>
+        </main><!-- #main .site-main -->
+    </div><!-- #primary .content-area -->
+</div><!-- .wrap -->
+<?php get_footer();
