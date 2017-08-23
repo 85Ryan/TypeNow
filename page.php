afcd990f705f183616a47965f3f593ad956af9ed
iiiRyan <85ryan.blue@gmail.com>
2017-08-23 15:59:31 +0800

    Fix Syntax error.

--- page.php
+++ page.php
@@ -7,7 +7,7 @@ get_header(); ?>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
             <?php 
-            while ( have_posts() ) : the_post() :
+            while ( have_posts() ) : the_post();
                 get_template_part( 'template-parts/page/content', 'page' );
             
                 // If comments are open or we have at least one comment, load up the comment template.
