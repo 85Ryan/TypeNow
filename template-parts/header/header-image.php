c61656b0cc435d34aff0d83654deb5af0d9b11ed
iiiRyan <85ryan.blue@gmail.com>
2017-08-23 13:22:55 +0800

    Fix syntax error in line#19.

--- template-parts/header/header-image.php
+++ template-parts/header/header-image.php
@@ -16,7 +16,7 @@
                 <?php if ( is_front_page() ) : ?>
                     <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
                 <?php else : ?>
-                    <p class="site-title"><a href="<?php echo_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                    <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
                 <?php endif; ?>
 
                 <?php 
