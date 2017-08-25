eff3fc9903b14ac4ce51bd806ccd0359419ef2ea
iiiRyan <85ryan.blue@gmail.com>
2017-08-25 21:42:54 +0800

    Searchform

--- searchform.php
+++ searchform.php
@@ -7,3 +7,14 @@
  * @version:    1.0
  */
 ?>
+
+<?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <label for="<?php echo $unique_id; ?>">
+		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'typenow' ); ?></span>
+	</label>
+
+	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'typenow' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+
+	<button type="submit" class="search-submit"><?php echo typenow_get_svg( array( 'icon' => 'search' ) ); ?><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'typenow' ); ?></span></button>
+</form>
