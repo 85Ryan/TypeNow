9f4a5813f5f637a117ebbdc1b2da267a15d4c294
iiiRyan <85ryan.blue@gmail.com>
2017-08-25 17:27:30 +0800

    Customize Preview

---
+++ assets/js/customize-preview.js
@@ -0,0 +1,114 @@
+/**
+ * File customize-preview.js.
+ *
+ * Instantly live-update customizer settings in the preview for improved user experience.
+ */
+
+(function( $ ) {
+
+	// Collect information from customize-controls.js about which panels are opening.
+	wp.customize.bind( 'preview-ready', function() {
+
+		// Initially hide the theme option placeholders on load
+		$( '.panel-placeholder' ).hide();
+
+		wp.customize.preview.bind( 'section-highlight', function( data ) {
+
+			// Only on the front page.
+			if ( ! $( 'body' ).hasClass( 'typenow-front-page' ) ) {
+				return;
+			}
+
+			// When the section is expanded, show and scroll to the content placeholders, exposing the edit links.
+			if ( true === data.expanded ) {
+				$( 'body' ).addClass( 'highlight-front-sections' );
+				$( '.panel-placeholder' ).slideDown( 200, function() {
+					$.scrollTo( $( '#panel1' ), {
+						duration: 600,
+						offset: { 'top': -70 } // Account for sticky menu.
+					});
+				});
+
+			// If we've left the panel, hide the placeholders and scroll back to the top.
+			} else {
+				$( 'body' ).removeClass( 'highlight-front-sections' );
+				// Don't change scroll when leaving - it's likely to have unintended consequences.
+				$( '.panel-placeholder' ).slideUp( 200 );
+			}
+		});
+	});
+
+	// Site title and description.
+	wp.customize( 'blogname', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-title a' ).text( to );
+		});
+	});
+	wp.customize( 'blogdescription', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-description' ).text( to );
+		});
+	});
+
+	// Display blog title.
+	wp.customize( 'typenow_display_title', function( value ) {
+		value.bind( function( to ) {
+			if ( false === to ) {
+				$( '.site-title' ).css({
+					clip: 'rect(1px, 1px, 1px, 1px)',
+					position: 'absolute'
+				});
+				// Add class for different logo styles if title and description are hidden.
+				$( 'body' ).addClass( 'title-hidden' );
+			} else {
+				$( '.site-title' ).css({
+					clip: 'auto',
+					position: 'relative'
+				});
+				// Add class for different logo styles if title and description are visible.
+				$( 'body' ).removeClass( 'title-hidden' );
+			}
+		});
+	});
+
+	// Display blog tagline.
+	wp.customize( 'typenow_display_tagline', function( value ) {
+		value.bind( function( to ) {
+			if ( false === to ) {
+				$( '.site-description' ).css({
+					clip: 'rect(1px, 1px, 1px, 1px)',
+					position: 'absolute'
+				});
+				// Add class for different logo styles if title and description are hidden.
+				$( 'body' ).addClass( 'tagline-hidden' );
+			} else {
+				$( '.site-description' ).css({
+					clip: 'auto',
+					position: 'relative'
+				});
+				// Add class for different logo styles if title and description are visible.
+				$( 'body' ).removeClass( 'tagline-hidden' );
+			}
+		});
+	});
+
+	// Whether a header image is available.
+	function hasHeaderImage() {
+		var image = wp.customize( 'header_image' )();
+		return '' !== image && 'remove-header' !== image;
+	}
+
+	// Toggle a body class if a custom header exists.
+	$.each( [ 'header_image' ], function( index, settingId ) {
+		wp.customize( settingId, function( setting ) {
+			setting.bind(function() {
+				if ( hasHeaderImage() ) {
+					$( document.body ).addClass( 'has-header-image' );
+				} else {
+					$( document.body ).removeClass( 'has-header-image' );
+				}
+			} );
+		} );
+	} );
+
+} )( jQuery );
