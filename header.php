1582ee288917300d00c703db76a97436356e2e22
iiiRyan <85ryan.blue@gmail.com>
2017-09-01 21:41:04 +0800

    BootCDN highlight CSS

--- header.php
+++ header.php
@@ -8,6 +8,8 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link href="https://cdn.bootcss.com/highlight.js/9.12.0/styles/xcode.min.css" rel="stylesheet">
+
     
     <?php wp_head(); ?>
 </head>
