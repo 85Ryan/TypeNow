4a3910d2d3ea786eb868da5372808be0b7fb39b8
iiiRyan <85ryan.blue@gmail.com>
2017-09-05 10:20:17 +0800

    BootCDN heighlight.js

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link href="https://cdn.bootcss.com/highlight.js/9.12.0/styles/xcode.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.bootcss.com/highlight.js/9.12.0/styles/xcode.min.css" rel="stylesheet"> -->
 
     
     <?php wp_head(); ?>
