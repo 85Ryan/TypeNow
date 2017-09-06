d53963a17752cee4fe5df82040b4c97e57a64710
iiiRyan <85ryan.blue@gmail.com>
2017-09-06 16:00:00 +0800

    fix page edit link and page links

--- template-parts/page/content-page.php
+++ template-parts/page/content-page.php
@@ -6,16 +6,18 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<?php typenow_edit_link( get_the_ID() ); ?>
 	</header><!-- .entry-header -->
 	<div class="entry-content">
 		<?php
 			the_content();
 
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'typenow' ),
-				'after'  => '</div>',
+				'before'             => '<div class="page-links">',
+				'after'              => '</div>',
+                'next_or_number'     => 'next',
+                'link_before'        => '',
+                'link_after'         => '',
 			) );
 		?>
 	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+</article><!-- #post -->
