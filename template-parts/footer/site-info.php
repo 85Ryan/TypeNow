76758ac6dfd253368c76d632eb402fcdb8ef1106
iiiRyan <85ryan.blue@gmail.com>
2017-09-06 15:58:44 +0800

    site info

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -1,9 +1,42 @@
 <?php 
 /**
  * Displays footer site info.
+ *
+ * @package:    Typenow
+ * @since:      1.0
+ * @version:    1.0
  */
 ?>
 
 <div class="site-info">
-    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'typenow' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'typenow' ), 'WordPress' ); ?></a>
+
+    <span class="site-copyright">
+       <?php typenow_copyright(); ?>
+    </span><!-- .site-copyright -->
+
+    <span class="site-icp">
+        <a href="<?php echo esc_url( __( 'http://www.miibeian.gov.cn/', 'typenow' ) ); ?>"><?php printf( __( 'ICP 12345678', 'typenow' ) ); ?></a>
+    </span><!-- .site-icp -->
+
+    <span class="site-map">
+        <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'typenow' ) ); ?>"><?php printf( __( 'Site Map', 'typenow' ) ); ?></a>
+    </span><!-- .site-map -->
+
+    <span class="site-theme">
+        <?php
+            $urltheme = 'https://github.com/85Ryan/TypeNow';
+            $urlauthor = 'https://iiiryan.com';
+            $sitetheme = sprintf( wp_kses( __( 'Theme is <a href="%1$s">TypeNow</a> by <a href="%2$s">Ryan</a>', 'typenow' ), array(  'a' => array( 'href' => array() ) ) ), esc_url( $urltheme ), esc_url( $urlauthor ) );
+            echo $sitetheme;
+        ?>
+    </span><!-- .site-theme -->
+
+    <span class="site-power">
+        <?php
+            $url = 'https://wordpress.org';
+            $sitepower = sprintf( wp_kses( __( 'Powerd by <a href="%s">WordPress</a>', 'typenow' ), array(  'a' => array( 'href' => array() ) ) ), esc_url( $url ) );
+            echo $sitepower;
+        ?>
+    </span><!-- .site-power -->
+
 </div><!-- .site-info -->
