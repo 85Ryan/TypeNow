d6a83d8b9ba3f08b2955fadf7454daaee1d9b428
iiiRyan <85ryan.blue@gmail.com>
2017-09-07 20:36:44 +0800

    fix comment form

--- comments.php
+++ comments.php
@@ -1,6 +1,10 @@
 <?php 
 /** 
  * The template for displaying comments.
+ *
+ * @package:    TypeNow
+ * @since:      1.0
+ * @version:    1.0
  */
 
 if ( post_password_required() ) {
@@ -9,35 +13,15 @@ if ( post_password_required() ) {
 ?>
 <div id="comments" class="comments area">
     <?php 
+	comment_form();
     if ( have_comments() ) : ?>
-        <h2 class="comments title">
-            <?php 
-            $comments_number = get_comments_number();
-            if ( '1' === $comments_number ) {
-                printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'typenow' ), get_the_title() );
-            } else {
-                printf(
-                    _nx(
-						'%1$s Reply to &ldquo;%2$s&rdquo;',
-						'%1$s Replies to &ldquo;%2$s&rdquo;',
-						$comments_number,
-						'comments title',
-						'twentyseventeen'
-					),
-                    number_format_i18n( $comments_number ),
-                    get_the_title()
-                );
-            }
-            ?>
-        </h2>
-        
         <ol class="comment-list">
             <?php
 				wp_list_comments( array(
-					'avatar_size' => 100,
+					'avatar_size' => 40,
 					'style'       => 'ol',
 					'short_ping'  => true,
-					'reply_text'  => typenow_get_svg( array( 'icon' => 'mail-reply' ) ) . __( 'Reply', 'typenow' ),
+					'reply_text'  => typenow_get_svg( array( 'icon' => 'comment-reply' ) ) . __( 'Reply', 'typenow' ),
 				) );
 			?>
         </ol>
@@ -45,9 +29,8 @@ if ( post_password_required() ) {
         <?php the_comments_pagination( array(
 			'prev_text' => typenow_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous', 'typenow' ) . '</span>',
 			'next_text' => '<span class="screen-reader-text">' . __( 'Next', 'typenow' ) . '</span>' . typenow_get_svg( array( 'icon' => 'arrow-right' ) ),
+            'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'typenow' ) . ' </span>',
 		) );
     endif;
-        
-    comment_form();
     ?>
 </div><!-- #comments .comments-area -->
