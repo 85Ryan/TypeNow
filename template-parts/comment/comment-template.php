d89a6da0248c7fa7ee00048dd28b7b48abb66d74
iiiRyan <85ryan.blue@gmail.com>
2017-09-07 20:42:28 +0800

    comment template

--- template-parts/comment/comment-template.php
+++ template-parts/comment/comment-template.php
@@ -7,3 +7,9 @@
  * @version:    1.0
  */
 ?>
+
+<div id="site-comment" class="comment-container">
+    <div class="wrap">
+        <?php comments_template(); ?>
+    </div><!-- .wrap -->
+</div><!-- #site-comment .comment-container -->
