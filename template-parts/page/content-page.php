fd299909390d985a7322fc996b3a416bf7d9af7e
iiiRyan <85ryan.blue@gmail.com>
2017-09-09 16:53:00 +0800

    add version info

--- template-parts/page/content-page.php
+++ template-parts/page/content-page.php
@@ -1,6 +1,10 @@
 <?php 
 /**
  * Template part for displaying page content in page.php.
+ *
+ * @package:    TypeNow
+ * @since:      1.0
+ * @version:    1.0
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
