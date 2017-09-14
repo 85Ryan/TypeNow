bb31ac7fbae6daf24aafcdcf38525e4d64508f1b
iiiRyan <85ryan.blue@gmail.com>
2017-09-14 11:51:12 +0800

    fix top nav right icons.

--- template-parts/navigation/navigation-top.php
+++ template-parts/navigation/navigation-top.php
@@ -22,13 +22,21 @@
         'depth'     => 1,
     ) ); ?>
     
-    <a class="menu-search-icon" href="<?php echo wp_login_url( home_url() ); ?>"><?php echo typenow_get_svg( array( 'icon' => 'search' ) ); ?><span class="screen-reader-text"><?php _e( 'Search', 'typenow' ); ?></span></a>
+    <div class="nav-icon-container">
+        <div class="search-icon-container">
+            <a class="menu-search-icon" href="<?php echo wp_login_url( home_url() ); ?>"><?php echo typenow_get_svg( array( 'icon' => 'search' ) ); ?><span class="screen-reader-text"><?php _e( 'Search', 'typenow' ); ?></span></a>
+        </div><!-- .search-icon-container -->
 
-    <?php if ( ! is_user_logged_in() ) : ?>
-        <a class="menu-login-icon" href="<?php echo wp_login_url( home_url() ); ?>"><?php echo typenow_get_svg( array( 'icon' => 'login' ) ); ?><span class="screen-reader-text"><?php _e( 'Login', 'typenow' ); ?></span></a>
-    <?php endif; ?>
+        <?php if ( ! is_user_logged_in() ) : ?>
+            <div class="login-icon-container">
+                <a class="menu-login-icon" href="<?php echo wp_login_url( home_url() ); ?>"><?php echo typenow_get_svg( array( 'icon' => 'login' ) ); ?><span class="screen-reader-text"><?php _e( 'Login', 'typenow' ); ?></span></a>
+            </div><!-- .login-icon-container -->
+        <?php endif; ?>
 
-    <?php if ( ( typenow_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
-        <a href="#content" class="menu-scroll-down"><?php echo typenow_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll Down to Content', 'typenow' ); ?></span></a>
-    <?php endif; ?>
+        <?php if ( ( typenow_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
+            <div class="scroll-icon-container">
+                <a href="#content" class="menu-scroll-down"><?php echo typenow_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll Down to Content', 'typenow' ); ?></span></a>
+            </div><!-- .scroll-icon-container -->
+        <?php endif; ?>
+    </div><!-- .nav-icon-container -->
 </nav><!-- #site-navigation .main-navigation -->
