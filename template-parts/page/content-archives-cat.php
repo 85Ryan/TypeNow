2af3340a914bce9ac3180a52310e9270c2247c59
iiiRyan <85ryan.blue@gmail.com>
2017-09-16 11:47:38 +0800

    add new file.

---
+++ template-parts/page/content-archives-cat.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template part for displaying archives by category.
+ *
+ * @package:    TypeNow
+ * @since:      1.0
+ * @version:    1.0
+ */
+?>
---
+++ template-parts/page/content-archives-time.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template part for displaying archives by timeline.
+ *
+ * @package:    TypeNow
+ * @since:      1.0
+ * @version:    1.0
+ */
+?>
