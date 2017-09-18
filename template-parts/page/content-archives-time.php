fd086c07d077be531e003897d2b00053906b5f33
iiiRyan <85ryan.blue@gmail.com>
2017-09-18 10:38:18 +0800

    archives by time

--- template-parts/page/content-archives-time.php
+++ template-parts/page/content-archives-time.php
@@ -7,3 +7,29 @@
  * @version:    1.0
  */
 ?>
+<?php
+
+    $ref_month = '';
+    $monthly = new WP_Query(array(
+        'posts_per_page' => -1,
+        'ignore_sticky_posts' => true
+    ));
+    if( $monthly->have_posts() ) :
+        while( $monthly->have_posts() ) : $monthly->the_post();
+
+    if( get_the_date('mY') != $ref_month ) {
+        $archive_year  = get_the_time('Y');
+        $archive_month = get_the_time('m');
+        if( $ref_month ) echo '</ul></div>';
+        echo '<div class="archives-title"><a href="'.get_month_link( $archive_year, $archive_month ).'">'.date('F Y',get_the_time('U')).'</a></div>';
+        echo '<div class="post-list-container"><ul class="post-list-body">';
+        $ref_month = get_the_date('mY');
+        $counter = 0;
+    }
+
+    echo '<li class="post-list-item"><a href='.get_permalink($post->ID).'><div class="item-label"><div class="item-title">'.get_the_title($post->ID).'</div><span class="item-date">'.typenow_get_svg(array('icon' => 'time')).date('M jS, Y', get_the_time('U')).'</span></div></a></li>';
+
+    endwhile;
+    echo '</ul></div>';
+    endif;
+?>
