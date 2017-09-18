254e1e39fc0c47ae6e2cc0c6dfca7424fe7a68b9
iiiRyan <85ryan.blue@gmail.com>
2017-09-18 10:38:07 +0800

    archives by category

--- template-parts/page/content-archives-cat.php
+++ template-parts/page/content-archives-cat.php
@@ -7,3 +7,26 @@
  * @version:    1.0
  */
 ?>
+<?php
+    global $cat;
+    $cats = get_categories(array(
+        'child_of' => $cat,
+        'parent' => $cat,
+        'hide_empty' => 0
+    ));
+    foreach($cats as $the_cat){
+        $posts = get_posts(array(
+            'category' => $the_cat->cat_ID,
+            'numberposts' => -1,
+        ));
+        if( !empty( $posts ) ) {
+            echo '<div class="archives-title"><a  href="'.get_category_link($the_cat).'">'.$the_cat->name.'</a></div><div class="post-list-container"><ul class="post-list-body">';
+
+            foreach( $posts as $post ){
+                echo '<li class="post-list-item"><a href="'.get_permalink($post->ID).'"><div class="item-label"><div class="item-title">'.$post->post_title.'</div><span class="item-date">'.typenow_get_svg(array('icon' => 'time')).date('M jS, Y', get_the_time('U')).'</span></div></a></li>';
+            }
+
+            echo '</ul></div>';
+        }
+    }
+?>
