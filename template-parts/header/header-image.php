0a83c04b99b42c9fa6aa882b53413c26dfdd8835
85Ryan <85ryan.blue@gmail.com>
2017-10-01 19:47:16 +0800

    fix header icon

--- template-parts/header/header-image.php
+++ template-parts/header/header-image.php
@@ -34,8 +34,21 @@
                     <p class="site-description"><?php echo $description; ?></p>
                 <?php endif; ?>
             </div><!-- .site-branding-text -->
-            <?php if ( ( typenow_is_frontpage() || ( is_home() && is_front_page() ) ) && ! has_nav_menu( 'top' ) ) : ?>
-            <a href="#content" class="menu-scroll-down"><?php echo typenow_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll Down to Content', 'typenow' ); ?></span></a>
+            <?php if ( ( typenow_is_frontpage() || ( is_home() && is_front_page() ) ) && ! has_nav_menu( 'top' ) && has_custom_header() ) : ?>
+                <div class="nav-icon-container">
+                    <div class="search-icon-container">
+                        <a class="menu-search-icon" href="http://iiiryan.dev/search/"><?php echo typenow_get_svg( array( 'icon' => 'search' ) ); ?><span class="screen-reader-text"><?php _e( 'Search', 'typenow' ); ?></span></a>
+                    </div><!-- .search-icon-container -->
+
+                    <?php if ( ! is_user_logged_in() ) : ?>
+                        <div class="login-icon-container">
+                            <a class="menu-login-icon" href="<?php echo wp_login_url( home_url() ); ?>"><?php echo typenow_get_svg( array( 'icon' => 'login' ) ); ?><span class="screen-reader-text"><?php _e( 'Login', 'typenow' ); ?></span></a>
+                        </div><!-- .login-icon-container -->
+                    <?php endif; ?>
+
+                    <div class="scroll-icon-container">
+                        <a href="#content" class="menu-scroll-down"><?php echo typenow_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll Down to Content', 'typenow' ); ?></span></a>
+                    </div><!-- .scroll-icon-container -->
             <?php endif; ?>
         </div><!-- .wrap -->
     </div><!-- .site-branding -->
