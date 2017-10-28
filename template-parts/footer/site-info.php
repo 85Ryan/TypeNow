5b135c9166f41e5b71b8d5f264743b1da89f225f
85Ryan <85ryan.blue@gmail.com>
2017-10-28 23:00:08 +0800

    add copyright notice

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -13,14 +13,17 @@
     <span class="site-copyright">
        <?php typenow_copyright(); ?>
     </span><!-- .site-copyright -->
+    <?php if ( get_theme_mod('typenow_site_icp', '') != '' ) : ?>
+        <span class="site-icp">
+            <a href="<?php echo esc_url( __( 'http://www.miibeian.gov.cn/', 'typenow' ) ); ?>"><?php printf( __( 'ICP ', 'typenow' ) ); ?><?php echo get_theme_mod('typenow_site_icp', ''); ?></a>
+        </span><!-- .site-icp -->
+    <?php endif; ?>
 
-    <span class="site-icp">
-        <a href="<?php echo esc_url( __( 'http://www.miibeian.gov.cn/', 'typenow' ) ); ?>"><?php printf( __( 'ICP 12345678', 'typenow' ) ); ?></a>
-    </span><!-- .site-icp -->
-
-    <span class="site-map">
-        <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'typenow' ) ); ?>"><?php printf( __( 'Site Map', 'typenow' ) ); ?></a>
-    </span><!-- .site-map -->
+    <?php if ( get_theme_mod('typenow_site_map', '') != '' ) : ?>
+        <span class="site-map">
+            <a href="<?php echo get_theme_mod('typenow_site_map', ''); ?>"><?php printf( __( 'Site Map', 'typenow' ) ); ?></a>
+        </span><!-- .site-map -->
+    <?php endif; ?>
 
     <span class="site-theme">
         <?php
@@ -38,5 +41,11 @@
             echo $sitepower;
         ?>
     </span><!-- .site-power -->
+
+     <?php if ( get_theme_mod('typenow_copy_notice', '') != '' ) : ?>
+        <p class="site-copy-notice">
+            <?php echo get_theme_mod('typenow_copy_notice', ''); ?>
+        </p><!-- .site-copy-notice -->
+    <?php endif; ?>
 
 </div><!-- .site-info -->
