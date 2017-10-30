0993f63ac7777ac60a79e2f332b1f8cf707ec106
85Ryan <85ryan.blue@gmail.com>
2017-10-30 15:55:19 +0800

    Define the related posts.

--- functions.php
+++ functions.php
@@ -252,6 +252,97 @@ function typenow_excerpt_length( $length ) {
 }
 add_filter( 'excerpt_length', 'typenow_excerpt_length', 999 );
 
+/**
+ * Define the related posts.
+ */
+// Get the thumbnail src.
+function typenow_post_thumbnail_src(){
+    global $post;
+	if ( has_post_thumbnail() ){
+        $thumbnail_src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID),'medium');
+		$post_thumbnail_src = $thumbnail_src [0];
+    } else {
+		$post_thumbnail_src = '';
+		ob_start();
+		ob_end_clean();
+        $output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+        if ( $output ) {
+            $post_thumbnail_src = $matches [1][0];
+        } else {
+            $post_thumbnail_src = ''.bloginfo('template_url').'/assets/images/related-post-default.jpg';
+        }
+	}
+	return $post_thumbnail_src;
+}
+
+// Define related posts.
+function typenow_related_post( $post_id ) {
+    global $post, $posts;
+
+    $post_num = get_theme_mod('typenow_related_post_num', '');
+    $exclude_id = $post->ID;
+    $posttags = get_the_tags(); $i = 0;
+
+    echo '<div class="related-post-container"><ul class="related-post-content">';
+
+    if ( $posttags ) {
+
+        $tags = ''; foreach ( $posttags as $tag ) $tags .= $tag->term_id . ',';
+        $args = array(
+            'post_status' => 'publish',
+            'tag__in' => explode(',', $tags),
+            'post__not_in' => explode(',', $exclude_id),
+            'ignore_sticky_posts' => 1,
+            'orderby' => 'comment_date',
+            'posts_per_page' => $post_num
+        );
+        query_posts($args);
+        while( have_posts() ) { the_post(); ?>
+            <li class="related-post-list">
+                <a href="<?php the_permalink(); ?>">
+                <div class="related-post-thumb" style="background-image:url(<?php echo typenow_post_thumbnail_src(); ?>)">
+                </div></a>
+                <div class="related-post-title">
+                    <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+                </div>
+                <div class="related-post-time">
+                    <a href="<?php the_permalink(); ?>"><?php echo typenow_get_svg(array('icon' => 'time')); ?><?php echo date('M jS, Y', get_the_time('U')); ?></a>
+                </div>
+            </li>
+        <?php
+            $exclude_id .= ',' . $post->ID; $i ++;
+        } wp_reset_query();
+    }
+
+    if ( $i < $post_num ) {
+        $cats = ''; foreach ( get_the_category() as $cat ) $cats .= $cat->cat_ID . ',';
+        $args = array(
+            'category__in' => explode(',', $cats),
+            'post__not_in' => explode(',', $exclude_id),
+            'ignore_sticky_posts' => 1,
+            'orderby' => 'comment_date',
+            'posts_per_page' => $post_num - $i
+        );
+        query_posts($args);
+        while( have_posts() ) { the_post(); ?>
+            <li class="related-post-list">
+                <a href="<?php the_permalink(); ?>">
+                <div class="related-post-thumb" style="background-image:url(<?php echo typenow_post_thumbnail_src(); ?>)">
+                </div></a>
+                <div class="related-post-title">
+                    <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+                </div>
+                <div class="related-post-time">
+                    <a href="<?php the_permalink(); ?>"><?php echo typenow_get_svg(array('icon' => 'time')); ?><?php echo date('M jS, Y', get_the_time('U')); ?></a>
+                </div>
+            </li>
+        <?php $i++;
+        } wp_reset_query();
+    }
+
+    echo '</ul></div>';
+}
+
 /** 
  * Include template file.
  */
