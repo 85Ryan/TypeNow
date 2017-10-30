75be27ddbbe611e7a67ee0b487a35e98261b2407
85Ryan <85ryan.blue@gmail.com>
2017-10-30 15:55:45 +0800

    fix bug

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -15,7 +15,7 @@
     </span><!-- .site-copyright -->
     <?php if ( get_theme_mod('typenow_site_icp', '') != '' ) : ?>
         <span class="site-icp">
-            <a href="<?php echo esc_url( __( 'http://www.miibeian.gov.cn/', 'typenow' ) ); ?>"><?php printf( __( 'ICP ', 'typenow' ) ); ?><?php echo get_theme_mod('typenow_site_icp', ''); ?></a>
+            <?php printf( __( 'ICP ', 'typenow' ) ); ?><a href="<?php echo esc_url( __( 'http://www.miibeian.gov.cn/', 'typenow' ) ); ?>"><?php echo get_theme_mod('typenow_site_icp', ''); ?></a>
         </span><!-- .site-icp -->
     <?php endif; ?>
 
