e95e5d40e6cb9ceda715a3bc43404f2d93d9ca9c
85Ryan <85ryan.blue@gmail.com>
2017-10-30 15:54:12 +0800

    fix bugs

--- assets/js/customize-preview.js
+++ assets/js/customize-preview.js
@@ -38,18 +38,6 @@
 		});
 	});
 
-	// Site title and description.
-	wp.customize( 'blogname', function( value ) {
-		value.bind( function( to ) {
-			$( '.site-title a' ).text( to );
-		});
-	});
-	wp.customize( 'blogdescription', function( value ) {
-		value.bind( function( to ) {
-			$( '.site-description' ).text( to );
-		});
-	});
-
 	// Display blog title.
 	wp.customize( 'typenow_display_title', function( value ) {
 		value.bind( function( to ) {
