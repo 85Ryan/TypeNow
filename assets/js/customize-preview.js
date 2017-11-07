e1c7475b21244d7d5b5ab1c6ea2d0924168a5f25
85Ryan <85ryan.blue@gmail.com>
2017-11-07 21:38:35 +0800

    fix bugs

--- assets/js/customize-preview.js
+++ assets/js/customize-preview.js
@@ -6,35 +6,15 @@
 
 (function( $ ) {
 
-	// Collect information from customize-controls.js about which panels are opening.
-	wp.customize.bind( 'preview-ready', function() {
-
-		// Initially hide the theme option placeholders on load
-		$( '.panel-placeholder' ).hide();
-
-		wp.customize.preview.bind( 'section-highlight', function( data ) {
-
-			// Only on the front page.
-			if ( ! $( 'body' ).hasClass( 'typenow-front-page' ) ) {
-				return;
-			}
-
-			// When the section is expanded, show and scroll to the content placeholders, exposing the edit links.
-			if ( true === data.expanded ) {
-				$( 'body' ).addClass( 'highlight-front-sections' );
-				$( '.panel-placeholder' ).slideDown( 200, function() {
-					$.scrollTo( $( '#panel1' ), {
-						duration: 600,
-						offset: { 'top': -70 } // Account for sticky menu.
-					});
-				});
-
-			// If we've left the panel, hide the placeholders and scroll back to the top.
-			} else {
-				$( 'body' ).removeClass( 'highlight-front-sections' );
-				// Don't change scroll when leaving - it's likely to have unintended consequences.
-				$( '.panel-placeholder' ).slideUp( 200 );
-			}
+	// Site title and description.
+	wp.customize( 'blogname', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-title a' ).text( to );
+		});
+	});
+	wp.customize( 'blogdescription', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-description' ).text( to );
 		});
 	});
 
@@ -98,5 +78,26 @@
 			} );
 		} );
 	} );
+
+    // Display site owner.
+	wp.customize( 'typenow_site_owner', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-copyright a' ).text( to );
+		});
+	});
+
+    // Display site ICP.
+	wp.customize( 'typenow_site_icp', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-icp a' ).text( to );
+		});
+	});
+
+    // Display copy notice.
+	wp.customize( 'typenow_copy_notice', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-copy-notice' ).text( to );
+		});
+	});
 
 } )( jQuery );
